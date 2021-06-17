b99a1d0bf04cd81ce6827f63279763eea3ca8dc1
Oleg Artyomov <artyomov@gmail.com>
2021-06-17 15:53:05 +0500

    Remove unused code

--- lib/main.dart
+++ lib/main.dart
@@ -4,30 +4,6 @@ void main() {
   runApp(App());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      // 3.6.3.5 Поместите ваш виджет на место MyHomePage в Вашем проекте.
-      home: MyStatefullWidget(),
-    );
-  }
-}
-
 // 3.6.3.1 Внутри созданного ранее проекта в папке main.dart создайте новый
 //Stateless виджет.
 // 3.6.3.2 Назовите его MyFirstWidget.
