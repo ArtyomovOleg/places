6f7f44b1e2f708805cb1353dbd00a6bccfe4b0d9
Oleg Artyomov <artyomov@gmail.com>
2021-06-17 17:22:55 +0500

    Выполнил задание

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -10,6 +10,21 @@ class SightListScreen extends StatefulWidget {
 class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Hello!',
+              hintText: 'Hello!',
+            ),
+            autofocus: true,
+          ),
+        ),
+      ),
+      resizeToAvoidBottomInset: false,
+    );
   }
 }
