d63885e9af3b56d693eee2c0905aab884390143f
Oleg Artyomov <artyomov@gmail.com>
2021-06-17 16:11:22 +0500

    Добавил экран и назначил его стартовым

--- lib/main.dart
+++ lib/main.dart
@@ -1,99 +1,17 @@
 import 'package:flutter/material.dart';
 
+import 'ui/screen/sight_list_screen.dart';
+
 void main() {
   runApp(App());
 }
 
-// 3.6.3.1 Внутри созданного ранее проекта в папке main.dart создайте новый
-//Stateless виджет.
-// 3.6.3.2 Назовите его MyFirstWidget.
-class MyFirstWidget extends StatelessWidget {
-  // 3.6.3.4 Сделайте счетчик вызовов функции build() внутри виджета и выведите
-  // его с помощью print().
-  final counters = Counters();
-
-  MyFirstWidget({Key? key}) : super(key: key);
-
-  // 3.6.3.3 В методе build() возвратите следующее:
-  @override
-  Widget build(BuildContext context) {
-    counters.cntBuild++;
-    print('$runtimeType build: ${counters.cntBuild}');
-    print('context.runtimeType: ${context.runtimeType}');
-
-    return Container(
-      child: Center(
-        child: Text('Hello!'),
-      ),
-    );
-  }
-
-  // 3.6.4.5. Перейдем к рассмотрению контекста. В вашем Stateless виджет
-  // создайте метод без аргументов, который будет возвращать
-  // context.runtimeType. Получится ли реализовать подобное в данном виджете?
-  Type get myRuntimeType => runtimeType;
-}
-
-class Counters {
-  int cntBuild = 0;
-}
-
-// 3.6.3.7 Чему равно значение счетчика? Почему значение именно такое?
-// Ответ: Значение счетчика равно 1. Т.к. это виджет без состояния и все данные
-// перестраиваются заново.
-
-// 3.6.3.8 Сделайте второй виджет(уже на основе Stateful). Повторите пункты 3-7.
-// Ответ на вопрос изменился?
-// Ответ: Да. В памяти сохраняется текущее состояние счетчика.
-class MyStatefullWidget extends StatefulWidget {
-  MyStatefullWidget({Key? key}) : super(key: key);
-
-  @override
-  _MyStatefullWidgetState createState() => _MyStatefullWidgetState();
-
-  // 3.6.4.6. Проделайте предыдущий пункт в рамках Stateful. В чем разница?
-  Type get myRuntimeType => runtimeType;
-}
-
-class _MyStatefullWidgetState extends State<MyStatefullWidget> {
-  final counters = Counters();
-
-  @override
-  Widget build(BuildContext context) {
-    counters.cntBuild++;
-    print('$runtimeType build: ${counters.cntBuild}');
-    print('context.runtimeType: ${context.runtimeType}');
-
-    return Container(
-      child: Center(
-        child: Text('Hello!'),
-      ),
-    );
-  }
-}
-// 3.6.4.1. Переименуйте файл main.dart в start.dart. Запустите проект. Почему
-// результат именно таков?
-// Не найден файл с точкой входа в приложение.
-// Target file "lib/main.dart" not found.
-
-// 3.6.4.3. В файле main.dart создайте виджет приложения. Назовите его App.
 class App extends StatelessWidget {
-  // 3.6.4.4. В методе build() верните MaterialApp в поле home поставьте
-  // Stateless виджет из предыдущего домашнего задания(см. урок про виджеты).
-  // Рассмотрите его параметры. Задайте поле title. Запустите на Android. Где
-  // отобразится значение этого поля?
-  // Ответ: В описании написано, что отобразиться в "недавних" приложениях, а на
-  // iOS значение параметра не используется. К задаче приложу скриншот.
   @override
   Widget build(BuildContext context) {
-    final myFirstWidget = MyFirstWidget();
-    final myStatefullWidget = MyStatefullWidget();
-    // print('myRuntimeType: ${myFirstWidget.myRuntimeType.toString()}');
-    print('myRuntimeType: ${myStatefullWidget.myRuntimeType.toString()}');
     return MaterialApp(
       title: 'App',
-      // home: myFirstWidget,
-      home: myStatefullWidget,
+      home: SightListScreen(),
     );
   }
 }
---
+++ lib/ui/screen/sight_list_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SightListScreen extends StatefulWidget {
+  const SightListScreen({Key? key}) : super(key: key);
+
+  @override
+  _SightListScreenState createState() => _SightListScreenState();
+}
+
+class _SightListScreenState extends State<SightListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
