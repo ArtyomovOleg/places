2e31e42c4b20bb5e7664d871a7990865c4abd44a
Oleg Artyomov <artyomov@gmail.com>
2021-06-23 16:48:02 +0500

    4.6.2

--- lib/ui/screen/sight_card.dart
+++ lib/ui/screen/sight_card.dart
@@ -3,74 +3,120 @@ import 'package:flutter/material.dart';
 import 'package:places/domain/sight.dart';
 
 class SightCard extends StatelessWidget {
-  const SightCard(Sight sight, {Key? key}) : super(key: key);
+  final Sight sight;
+
+  SightCard(this.sight, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Row(children: [
-        Container(
-          color: Color(0xFFFFFFFF),
-          width: 361,
-          height: 360,
-          padding: const EdgeInsets.symmetric(horizontal: 0.5, vertical: 0),
-          child: Row(children: [
-            Container(
-              width: 288,
-              height: 360,
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Color(0xFFDD0000),
-                    Color(0xFFDDDD11),
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+      child: Container(
+        height: 96 + 92,
+        width: double.infinity,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Flexible(
+              flex: 96,
+              child: Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color(0xFF0000DD),
+                      Color(0xFFDD11DD),
+                    ],
+                    begin: Alignment.centerLeft,
+                    end: Alignment(0, 0.6),
+                  ),
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(16),
+                  ),
+                ),
+                child: Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    Positioned(
+                      left: 16,
+                      top: 16,
+                      child: Text(
+                        sight.type.toString().split('.')[1],
+                        style: const TextStyle(
+                          color: Color(0xFFFFFFFF),
+                          fontSize: 14.0,
+                          height: 1.28571,
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      right: 18,
+                      top: 19,
+                      child: Icon(
+                        Icons.star_border_rounded,
+                        size: 24,
+                      ),
+                    ),
                   ],
-                  begin: Alignment.topCenter,
-                  end: Alignment(0.6, 0),
                 ),
               ),
             ),
-            Container(
-              width: 288,
-              height: 360,
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Color(0xFF0000DD),
-                    Color(0xFFDD11DD),
+            Flexible(
+              flex: 92,
+              child: Container(
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.vertical(
+                    bottom: Radius.circular(16),
+                  ),
+                  color: Color(0xFFF5F5F5),
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(16, 16, 16, 2),
+                      child: Container(
+                        width: double.infinity,
+                        child: Text(
+                          sight.name,
+                          maxLines: 2,
+                          style: const TextStyle(
+                            color: Color(0xFF3B3E5B),
+                            fontSize: 16.0,
+                            height: 1.25,
+                            fontFamily: 'Roboto',
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(16, 0, 16, 16),
+                      child: Align(
+                        alignment: Alignment.topLeft,
+                        child: FittedBox(
+                          fit: BoxFit.fitHeight,
+                          child: Text(
+                            sight.details,
+                            style: const TextStyle(
+                              color: Color(0xFF7C7E92),
+                              fontSize: 14.0,
+                              height: 1.28571,
+                              fontFamily: 'Roboto',
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
                   ],
-                  begin: Alignment.centerLeft,
-                  end: Alignment(0, 0.6),
                 ),
               ),
             ),
-          ]),
+          ],
         ),
-      ]),
-      Container(
-        padding: EdgeInsets.symmetric(vertical: 24, horizontal: 16),
-        child: Column(children: [
-          Text('Пряности и радости'),
-          Padding(
-            padding: const EdgeInsets.only(top: 2),
-            child: Row(children: [
-              Text('ресторан'),
-              Padding(
-                padding: const EdgeInsets.only(left: 16),
-                child: Text('закрыто до 09:00'),
-              ),
-            ]),
-          )
-        ]),
       ),
-      Container(
-        padding: const EdgeInsets.only(bottom: 24, left: 16, right: 16),
-        child: Text('Пряный вкус радостной жизни'),
-      ),
-      Container(
-        padding: const EdgeInsets.only(bottom: 24, left: 16, right: 16),
-        child: Text('Построить маршрут'),
-      ),
-      Divider(height: 8),
-    ]);
+    );
   }
 }
--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
 import 'package:places/mocks.dart';
-import 'package:places/domain/sight.dart';
+import 'package:places/ui/screen/sight_card.dart';
 
 class SightListScreen extends StatefulWidget {
   const SightListScreen({Key? key}) : super(key: key);
@@ -51,10 +51,13 @@ class _SightListScreenState extends State<SightListScreen> {
               children: [
                 for (final sight in mocks)
                   ListTile(
-                    title: SightPreview(sight),
+                    title: SightCard(sight),
                     contentPadding: const EdgeInsets.all(0),
                     horizontalTitleGap: 0,
                     minVerticalPadding: 0,
+                    onTap: () {
+                      print('Tap on: ${sight.name}');
+                    },
                   ),
               ],
             ),
@@ -75,84 +78,6 @@ class _SightListScreenState extends State<SightListScreen> {
         ),
       ),
       resizeToAvoidBottomInset: false,
-    );
-  }
-}
-
-class SightPreview extends StatelessWidget {
-  final Sight sight;
-
-  SightPreview(this.sight, {Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
-      child: Container(
-        height: 96 + 92,
-        width: double.infinity,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Flexible(
-              flex: 96,
-              child: Container(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [
-                      Color(0xFF0000DD),
-                      Color(0xFFDD11DD),
-                    ],
-                    begin: Alignment.centerLeft,
-                    end: Alignment(0, 0.6),
-                  ),
-                  borderRadius: BorderRadius.vertical(
-                    top: Radius.circular(16),
-                  ),
-                ),
-                child: Stack(
-                  fit: StackFit.expand,
-                  children: [
-                    Positioned(
-                      left: 16,
-                      top: 16,
-                      child: Text(
-                        sight.type.toString().split('.')[1],
-                        style: TextStyle(
-                          color: Color(0xFFFFFFFF),
-                          fontSize: 14.0,
-                          height: 1.28571,
-                          fontFamily: 'Roboto',
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      right: 18,
-                      top: 19,
-                      child: Icon(
-                        Icons.star_border_rounded,
-                        size: 24,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Flexible(
-              flex: 92,
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.vertical(
-                    bottom: Radius.circular(16),
-                  ),
-                  color: Color(0xFFF5F5F5),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
