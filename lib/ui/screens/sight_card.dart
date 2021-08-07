2112b23f6ecbc2856bc93bd04d923b6c35b4f643
Oleg Artyomov <artyomov@gmail.com>
2021-08-07 18:13:07 +0500

    5.6.1

--- lib/ui/screens/sight_card.dart
+++ lib/ui/screens/sight_card.dart
@@ -1,25 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 import 'package:places/domain/sight.dart';
+import 'package:places/ui/screens/res/svg_pictures.dart';
 import 'package:places/ui/screens/sight_details.dart';
 
 class SightCard extends StatefulWidget {
   final Sight sight;
-  final List<SvgPicture> iconsHeart = [
-    SvgPicture.asset('assets/icons/heart_unchecked.svg'),
-    SvgPicture.asset('assets/icons/heart_checked.svg'),
-  ];
+  final bool favorite;
+  final Function favoriteSwitchSight;
 
-  SightCard(this.sight, {Key? key}) : super(key: key);
+  SightCard(this.sight, this.favorite, this.favoriteSwitchSight, {Key? key})
+      : super(key: key);
 
   @override
   _SightCardState createState() => _SightCardState();
 }
 
 class _SightCardState extends State<SightCard> {
-  int heartIndex = 0;
-
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -29,136 +26,143 @@ class _SightCardState extends State<SightCard> {
       child: Material(
         child: InkWell(
           onTap: () {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text(
-                  'Tap on: ${widget.sight.name}',
-                  style: TextStyle(
-                    color: Theme.of(context).buttonColor,
-                  ),
-                ),
-                backgroundColor:
-                    Theme.of(context).scaffoldBackgroundColor.withAlpha(0x8F),
-                elevation: 3,
-              ),
-            );
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (BuildContext context) => SightDetails(widget.sight),
-              ),
-            );
+            action(context);
           },
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              Flexible(
-                flex: 3,
-                child: Ink(
-                  decoration: BoxDecoration(
-                    gradient: const LinearGradient(
-                      colors: [
-                        Color(0xFF0000DD),
-                        Color(0xFFDD11DD),
-                      ],
-                      begin: Alignment.centerLeft,
-                      end: Alignment(0, 0.6),
-                    ),
-                    borderRadius: const BorderRadius.vertical(
-                      top: Radius.circular(16),
-                    ),
-                  ),
-                  child: Stack(
-                    fit: StackFit.expand,
-                    children: [
-                      widget.sight.assetImagePath.isNotEmpty
-                          // ? Image.asset(sight.assetImagePath)
-                          ? imageFutureBuilder()
-                          : Container(),
-                      Positioned(
-                        left: 16,
-                        top: 16,
-                        child: Text(
-                          widget.sight.type.toString().split('.').last,
-                          style: Theme.of(context).textTheme.headline4,
-                        ),
-                      ),
-                      Positioned(
-                        right: 16,
-                        top: 16,
-                        child: IconButton(
-                          onPressed: () {
-                            print(
-                                '$runtimeType (${widget.sight.type}) heart icon has pressed');
-                            setState(() {
-                              heartIndex = (heartIndex - 1).abs();
-                            });
-                          },
-                          icon: widget.iconsHeart[heartIndex],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Flexible(
-                flex: 2,
-                child: Ink(
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.vertical(
-                      bottom: Radius.circular(16),
-                    ),
-                    color: Theme.of(context).cardColor,
-                  ),
-                  child: Column(
-                    children: [
-                      SizedBox(height: 16),
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(16, 0, 16, 2),
-                        child: Container(
-                          width: double.infinity,
-                          child: Text(
-                            widget.sight.name,
-                            maxLines: 2,
-                            style: Theme.of(context).textTheme.headline5,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(16, 0, 16, 16),
-                        child: Container(
-                          child: Align(
-                            alignment: Alignment.topLeft,
-                            child: LayoutBuilder(builder: (
-                              BuildContext context,
-                              BoxConstraints constraints,
-                            ) {
-                              return ConstrainedBox(
-                                constraints: BoxConstraints.loose(
-                                  Size(
-                                    constraints.maxWidth / 2,
-                                    21,
-                                  ),
-                                ),
-                                child: Text(
-                                  'Закрыто до 09:00',
-                                  overflow: TextOverflow.fade,
-                                  textAlign: TextAlign.left,
-                                  style: Theme.of(context).textTheme.subtitle2,
-                                ),
-                              );
-                            }),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+              Flexible(flex: 3, child: topPart(context)),
+              Flexible(flex: 2, child: bottomPart(context)),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  void action(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(
+          'Tap on: ${widget.sight.name}',
+          style: TextStyle(color: Theme.of(context).buttonColor),
+        ),
+        backgroundColor:
+            Theme.of(context).scaffoldBackgroundColor.withAlpha(0x8F),
+        elevation: 3,
+      ),
+    );
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (BuildContext context) => SightDetails(widget.sight),
+      ),
+    );
+  }
+
+  Ink bottomPart(BuildContext context) {
+    return Ink(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.vertical(bottom: Radius.circular(16)),
+        color: Theme.of(context).cardColor,
+      ),
+      child: Column(
+        children: [
+          SizedBox(height: 16),
+          Padding(
+            padding: EdgeInsets.fromLTRB(16, 0, 16, 2),
+            child: Container(
+              width: double.infinity,
+              child: Text(
+                widget.sight.name,
+                maxLines: 2,
+                style: Theme.of(context).textTheme.headline5,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(16, 0, 16, 16),
+            child: Container(
+              child: Align(
+                alignment: Alignment.topLeft,
+                child: LayoutBuilder(builder: (
+                  BuildContext context,
+                  BoxConstraints constraints,
+                ) {
+                  return ConstrainedBox(
+                    constraints: BoxConstraints.loose(
+                      Size(
+                        constraints.maxWidth / 2,
+                        21,
+                      ),
+                    ),
+                    child: Text(
+                      'Закрыто до 09:00',
+                      overflow: TextOverflow.fade,
+                      textAlign: TextAlign.left,
+                      style: Theme.of(context).textTheme.subtitle2,
+                    ),
+                  );
+                }),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Ink topPart(BuildContext context) {
+    return Ink(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color(0xFF0000DD),
+            Color(0xFFDD11DD),
+          ],
+          begin: Alignment.centerLeft,
+          end: Alignment(0, 0.6),
+        ),
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(16),
+        ),
+      ),
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          widget.sight.assetImagePath.isNotEmpty
+              // ? Image.asset(sight.assetImagePath)
+              ? imageFutureBuilder()
+              : Container(),
+          Positioned(
+            left: 16,
+            top: 16,
+            child: Text(
+              widget.sight.type.toString().split('.').last,
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ),
+          Positioned(
+            right: 16,
+            top: 16,
+            child: favoriteButton(context),
+          ),
+        ],
+      ),
+    );
+  }
+
+  IconButton favoriteButton(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        print(
+          '$runtimeType (${widget.sight.type}) heart icon has pressed',
+        );
+        widget.favoriteSwitchSight(widget.sight);
+      },
+      icon: widget.favorite
+          ? SvgIcon(context, 'HeartFill', color: Colors.white)
+          : SvgIcon(context, 'HeartOutline', color: Colors.white),
     );
   }
 
--- lib/ui/screens/visiting_screen.dart
+++ lib/ui/screens/visiting_screen.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:places/domain/sight.dart';
 
 import 'package:places/mocks.dart';
 import 'package:places/ui/screens/res/text_styles.dart';
 import 'package:places/ui/screens/sight_card_visiting.dart';
 
 class VisitingScreen extends StatefulWidget {
-  const VisitingScreen({Key? key}) : super(key: key);
+  final List<Sight> favoriteSights;
+  final Function favoriteSwitchSight;
+
+  const VisitingScreen(this.favoriteSights, this.favoriteSwitchSight,
+      {Key? key})
+      : super(key: key);
 
   @override
   _VisitingScreenState createState() => _VisitingScreenState();
@@ -54,7 +61,19 @@ class _VisitingScreenState extends State<VisitingScreen> {
             Column(
               children: [
                 SizedBox(height: 16),
-                SightCardWantVisit(sightMonument),
+                Expanded(
+                  child: widget.favoriteSights.length == 0
+                      ? titleEmptyList(context)
+                      : SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              for (Sight sight in widget.favoriteSights)
+                                SightCardWantVisit(
+                                    sight, widget.favoriteSwitchSight),
+                            ],
+                          ),
+                        ),
+                ),
               ],
             ),
             Column(
@@ -62,6 +81,41 @@ class _VisitingScreenState extends State<VisitingScreen> {
                 SizedBox(height: 16),
                 SightCardVisited(sightGraveyard),
               ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget titleEmptyList(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              'assets/icons/card.svg',
+              width: 64,
+              height: 64,
+              color: Theme.of(context).buttonTheme.colorScheme?.secondary,
+            ),
+            SizedBox(height: 24),
+            Text(
+              'Пусто',
+              textAlign: TextAlign.center,
+              style: addSightTopTitle.copyWith(
+                color: Theme.of(context).buttonTheme.colorScheme?.secondary,
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Отмечайте понравившиеся\nместа и они появиятся здесь.',
+              textAlign: TextAlign.center,
+              style: sightCardVisitingTitle.copyWith(
+                color: Theme.of(context).buttonTheme.colorScheme?.secondary,
+              ),
             ),
           ],
         ),
